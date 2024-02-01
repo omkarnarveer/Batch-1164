86b08c0511c32d54a37738a9c245324bc59289f1
Omkar Narveer <omkarnarveer07@gmail.com>
2024-02-01 11:04:40 +0530

    Initial Commit Day 3 Feb1 If Else Practice

---
+++ Batch 1164/src/feb_1/com/ifese/demo/EvenNumber.java
@@ -0,0 +1,14 @@
+package feb_1.com.ifese.demo;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+
+		int n=32;
+		if(n%2==0) {
+			System.out.println("Number: "+n+ " is Even No");
+		}
+		else
+			System.out.println("Number: "+n+ " is not Even No");
+	}
+}
---
+++ Batch 1164/src/feb_1/com/ifese/demo/MultipeOf.java
@@ -0,0 +1,9 @@
+package feb_1.com.ifese.demo;
+
+public class MultipeOf {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
